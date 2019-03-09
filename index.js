d6222a9d2c87d5297f511a7258afac0a6cd5a5c7
github username <github email address>
2019-03-09 12:15:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+// Your code herevar dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown', (e) => {
+  let hozPoz = Number.parseInt(dodger.style.left.replace("px", ""));
+
+	if (e.which === 37) {
+    hozPoz--;
+  } else if (e.which === 39) {
+    hozPoz++;
+  }
+  dodger.style.left = `${hozPoz}px`;
+});
+
+function moveDodgerLeft () {
+  let dodger = document.getElementById('dodger');
+  let hozPoz = Number.parseInt(dodger.style.left.replace("px", ""));
+
+  dodger.style.left = `${--hozPoz}px`;
+}
+
+function moveDodgerRight () {
+  let dodger = document.getElementById('dodger');
+  let hozPoz = Number.parseInt(dodger.style.left.replace("px", ""));
+
+  dodger.style.left = `${++hozPoz}px`;
+}
