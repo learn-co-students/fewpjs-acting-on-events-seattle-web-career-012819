943ce7632335d2c37b0e2abec35367b21435857c
dahby <dstoll243@gmail.com>
2019-03-11 13:16:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+console.log('loaded');
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.which);
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
+
+const moveDodgerLeft = () => {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  dodger.style.left = `${left - 1}px`
+}
+
+const moveDodgerRight = () => {
+  console.log('fired right');
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  dodger.style.left = `${left + 1}px`
+}
