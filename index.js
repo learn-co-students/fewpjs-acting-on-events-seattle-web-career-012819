6e87d12bb21e70d0f2e93e4c4d21833b1cbb0e3b
github username <github email address>
2019-03-11 17:17:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 39) {
+    moveDodgerRight()
+  }
+})
